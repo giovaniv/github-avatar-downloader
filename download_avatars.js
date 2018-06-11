043851ba2c97960dd1f8b6298cb0b9c4ffb3ede1
giovaniv <giovaniv@gmail.com>
2018-06-11 21:34:43 +0000

    start coding getrepocontributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,2 +1,7 @@
 var request = require('request');
 console.log('Welcome to the Github Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  console.log('Errors:', err);
+  console.log('Result:', result);
+};
