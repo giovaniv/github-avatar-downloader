ad2e87bdd14751ade7314777c65a31106238d960
giovaniv <giovaniv@gmail.com>
2018-06-11 23:35:22 +0000

    added logic to retrieve only the avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -2,6 +2,32 @@ var request = require('request');
 console.log('Welcome to the Github Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  console.log('Errors:', err);
-  console.log('Result:', result);
-};
+
+  var options = {
+    url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers : {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options,function(err, res, body){
+    cb(err, body);
+  });
+
+}
+
+function showInfo(err, body) {
+  var content;
+  if (err) {
+    console.log(err);
+  }
+  content = JSON.parse(body);
+  content.forEach(function(item){
+    console.log(item.avatar_url);
+  });
+  //console.log(content.avatar_url);
+  console.log(content.length);
+}
+
+//getRepoContributors('nodejs','node',showInfo);
+getRepoContributors('lighthouse-labs','blockchain-resources',showInfo)
